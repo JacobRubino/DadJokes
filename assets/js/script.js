01a95b227696bf882112eed961115d958865714a
Jacob Rubino <jacobrubino@gmail.com>
2023-03-07 18:47:56 -0500

    not pushing these

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,23 @@ fetch('https://google-translate1.p.rapidapi.com/language/translate/v2/detect', o
 }
 testGoogleTranslate();
 
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://google-translate1.p.rapidapi.com/language/translate/v2",
+	"method": "POST",
+	"headers": {
+		"x-rapidapi-host": "google-translate1.p.rapidapi.com",
+		"x-rapidapi-key": "0b838a4d3fmsheccda4035a1ab37p1f5f85jsn1859900e2b60",
+		"content-type": "application/x-www-form-urlencoded"
+	},
+	"data": {
+		"source": "en",
+		"q": "Contact Form | Name | Contact Number | Email",
+		"target": ""
+	}
+	
+}
 //make a request in JSON format that has the phrase that we're translating. (dad joke), the source language (english), and the target language (what user chooses) as an object.
 
 //object-- key will be language user selects value will be google language that correspondes to user language chosen.
