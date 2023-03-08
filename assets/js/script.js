834f30c6d1343ff580ff9c2d77babde29f0c48af
Hailey Flora <Haileycflora@gmail.com>
2023-03-08 00:43:42 -0500

    add JokesStr[]

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+let jokesStr = [
+
+"What do you call a line of men waiting to get haircuts? A barberqueue.",
+
+"Where do pirates get their hooks? Second hand stores.",
+
+"I just broke up with my mathematician girlfriend. She was obsessed with an X.",
+
+"What does a baby computer call his father? Data.",
+
+"What do you call a beehive without an exit? Unbelievable.",
+
+"Did you know that the first french fries weren\'t cooked in France? They were cooked in Greece.",
+
+"It\'s easy to convince ladies not to eat Tide Pods, but harder to deter gents.",
+
+"I asked my date to meet me at the gym but she never showed up. I guess the two of us aren't going to work out.",
+
+"To whoever stole my copy of Microsoft Office, I will find you. You have my Word.",
+
+"What\'s Forrest Gump\'s password? 1forrest1.",
+
+"If prisoners could take their own mug shots…They\'d be called cellfies.",
+
+"What is a guitar player\'s favorite Italian food? Strum-boli.",
+
+"How does cereal pay its bills? With Chex.",
+
+"Have you heard about the restaurant on the moon? Great food, no atmosphere.",
+
+"I don\'t trust stairs. They\'re always up to something.",
+
+"Never date a tennis player. Love means nothing to them.",
+
+"What\'s a lawyer\'s favorite drink? Subpoena colada.",
+
+"I used to hate facial hair, but then it grew on me.",
+
+"I don\'t get why bakers aren\'t wealthier. They make so much dough.",
+
+"I hate it when people say age is only a number. Age is clearly a word.",
+
+"What\'s an astronaut\'s favorite part of the computer? The Space Bar.",
+
+"Today I\'m attaching a light to the ceiling, but I\'m afraid I\'ll probably screw it up.",
+
+"I just applied for a job down at the diner. I told them I really bring a lot to the table.",
+
+"Why was 2019 afraid of 2020? Because they had a fight and 2021.",
+
+"What has five toes and isn\'t your foot? My foot.",
+
+"When I was a kid, my mother told me I could be anyone I wanted to be. Turns out, identity theft is a crime.",
+
+"What did one plate whisper to the other plate? Dinner is on me.",
+
+"Stop looking for the perfect match; use a lighter.",
+
+"Did you hear the rumor about butter? Well, I'm not going to go spreading it!",
+
+"What does a nosey pepper do? It gets jalapeño business.",
+
+"If two vegetarians get in an argument, is it still called beef?",
+
+"What concert would cost only 45 cents? 50 Cent featuring Nickelback!",
+
+"How do you deal with a fear of speed bumps? You slowly get over it.",
+
+"What did the baby corn say to the mama corn? Where's popcorn?",
+
+"What do you call two monkeys who share an Amazon Prime account? Prime mates.",
+
+"Why did the football coach go to the bank? To get his quarter back.Why can\'t a leopard hide? He's always spotted.",
+
+"How you fix a broken pumpkin? With a pumpkin patch!",
+
+"Can February March? No, but April May!",
+
+"Why are fish so smart? They live in schools!",
+
+"I\'m so good at sleeping that I do it with my eyes closed.",
+
+"I hated facial hair but then it grew on me.",
+
+"What do houses wear? An address.",
+
+"What kind of shoes does a lazy person wear? Loafers."
+
+]
